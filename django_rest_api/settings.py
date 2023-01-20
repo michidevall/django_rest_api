29d06ca7b6378043927a129cb45d1730519f001f
Michi Devall <85964463+michidevall@users.noreply.github.com>
2023-01-20 11:53:02 -0700

    second try

--- django_rest_api/settings.py
+++ django_rest_api/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
